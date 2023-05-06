1a7ab65d1f28638d8e0784a08d9505a4e5634a39
Masha <teenderrr@gmail.com>
2023-05-06 19:09:33 +0300

    feature/moile-refactoring-2

--- src/components/up-button/up-button.tsx
+++ src/components/up-button/up-button.tsx
@@ -12,7 +12,7 @@ export const UpButton = () => {
       className="up-button"
       onClick={handleClick}
     >
-      <span>^</span>
+      <span className="uop-button__arrow">^</span>
     </button>
   );
 };
