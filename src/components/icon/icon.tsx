66192b4d210166dd82e0a00b8e80bd5cccfab240
Masha <teenderrr@gmail.com>
2023-05-07 15:37:38 +0300

    feature/mobile-refactoring

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -3,12 +3,14 @@ import { ReactComponent as Github } from '../../assets/icons/github.svg';
 import { ReactComponent as Linkedin } from '../../assets/icons/linkedin.svg';
 import { ReactComponent as Gmail } from '../../assets/icons/gmail.svg';
 import { ReactComponent as Telegram } from '../../assets/icons/telegram.svg';
+import { ReactComponent as Arrow } from '../../assets/icons/arrow.svg';
 
 export const ICONS = {
   github: Github,
   gmail: Gmail,
   linkedin: Linkedin,
   telegram: Telegram,
+  arrow: Arrow,
 } as const;
 
 export type TIcon = keyof typeof ICONS;
